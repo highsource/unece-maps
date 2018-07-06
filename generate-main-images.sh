c3377d75127f586a374efac0c2014b6c07719436
Alexey Valikov <aleksei.valikov@gmail.com>
2018-07-06 23:27:19 +0200

    Adjusted the density coefficient.

--- generate-main-images.sh
+++ generate-main-images.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 mkdir -p images/European_inland_waterways_-_2012
-convert -verbose -density 9.309090909090909 data/European_inland_waterways_-_2012.pdf images/European_inland_waterways_-_2012/0.png
-convert -verbose -density 18.61818181818182 data/European_inland_waterways_-_2012.pdf images/European_inland_waterways_-_2012/1.png
-convert -verbose -density 37.23636363636364 data/European_inland_waterways_-_2012.pdf images/European_inland_waterways_-_2012/2.png
-convert -verbose -density 74.47272727272727 data/European_inland_waterways_-_2012.pdf images/European_inland_waterways_-_2012/3.png
-convert -verbose -density 148.9243005254935 data/European_inland_waterways_-_2012.pdf images/European_inland_waterways_-_2012/4.png
-convert -verbose -density 297.8486010509871 data/European_inland_waterways_-_2012.pdf images/European_inland_waterways_-_2012/5.png
-convert -verbose -density 595.6972021019742 data/European_inland_waterways_-_2012.pdf images/European_inland_waterways_-_2012/6.png
-convert -verbose -density 1191.394404203948 data/European_inland_waterways_-_2012.pdf images/European_inland_waterways_-_2012/7.png
+convert -verbose -density 7.326022470124202 data/European_inland_waterways_-_2012.pdf images/European_inland_waterways_-_2012/0.png
+convert -verbose -density 14.65204494024840 data/European_inland_waterways_-_2012.pdf images/European_inland_waterways_-_2012/1.png
+convert -verbose -density 29.30408988049681 data/European_inland_waterways_-_2012.pdf images/European_inland_waterways_-_2012/2.png
+convert -verbose -density 58.60817976099362 data/European_inland_waterways_-_2012.pdf images/European_inland_waterways_-_2012/3.png
+convert -verbose -density 117.2163595219872 data/European_inland_waterways_-_2012.pdf images/European_inland_waterways_-_2012/4.png
+convert -verbose -density 234.4327190439745 data/European_inland_waterways_-_2012.pdf images/European_inland_waterways_-_2012/5.png
+convert -verbose -density 468.8654380879489 data/European_inland_waterways_-_2012.pdf images/European_inland_waterways_-_2012/6.png
