a1acdc29ef2508275afac085badb022a96d5b7f8
Alexey Valikov <aleksei.valikov@gmail.com>
2018-07-06 20:52:38 +0200

    Added the generate main images script.

---
+++ generate-main-images.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+md images
+convert -verbose data/European_inland_waterways_-_2012.pdf -resize   256 images/European_inland_waterways_-_2012/0.png
+convert -verbose data/European_inland_waterways_-_2012.pdf -resize   512 images/European_inland_waterways_-_2012/1.png
+convert -verbose data/European_inland_waterways_-_2012.pdf -resize  1024 images/European_inland_waterways_-_2012/2.png
+convert -verbose data/European_inland_waterways_-_2012.pdf -resize  2048 images/European_inland_waterways_-_2012/3.png
+convert -verbose data/European_inland_waterways_-_2012.pdf -resize  4096 images/European_inland_waterways_-_2012/4.png
+convert -verbose data/European_inland_waterways_-_2012.pdf -resize  8192 images/European_inland_waterways_-_2012/5.png
+convert -verbose data/European_inland_waterways_-_2012.pdf -resize 16384 images/European_inland_waterways_-_2012/6.png
+convert -verbose data/European_inland_waterways_-_2012.pdf -resize 32768 images/European_inland_waterways_-_2012/7.png
